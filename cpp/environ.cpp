092dc3f22fde9edd10dee1786c66c21b16e6ce6d
Wesley George <wgeorge@bionym.com>
2015-12-13 23:28:00 -0500

    Simple test program for environment variable inspection.
    	I'd known about the `getenv` system call, but I didn't know how to enumerate all environment variables.
    
    	Some people talk about using the following signature for main:
    
    	`int main(int argc, char** argv, char** environp);`
    
    	This is unattractive, as that `environp` pointer isn't always in scope.
    
    	The solution here, using `extern char** environ` matches the experience of environment variables - that they're always around and available to your program.

---
+++ cpp/environ.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+extern char **environ;
+
+int main() {
+  int i = 1;
+  char *s = *environ;
+
+  for (; s; i++) {
+    printf("%s\n", s);
+    s = *(environ+i);
+  }
+
+  return 0;
+}
+
