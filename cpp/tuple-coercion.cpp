302778ffe99a115caec3d52f81a16a04d5ca0312
Wesley George <wgeorge@bionym.com>
2016-01-18 17:24:20 -0500

    Tuple coercion continued: how to choke the compiler.

--- cpp/tuple-coercion.cpp
+++ cpp/tuple-coercion.cpp
@@ -3,6 +3,13 @@
 
 typedef std::tuple<int, bool> MyTuple;
 
+class MyClass {
+public:
+	MyClass(int i) : i(i) {};
+private:
+	int i;
+};
+
 int main() {
 	MyTuple m;
 
@@ -14,4 +21,16 @@ int main() {
 	
 	m = 1;
 	std::cout << std::get<0>(m) << ", " << std::get<1>(m) << "\n";
+
+	// The following does compile.
+	m = false;
+	//	Here the compiler is following the types bool -> int -> std::tuple<int, bool>
+	std::cout << std::get<0>(m) << ", " << std::get<1>(m) << "\n";
+
+	// Uncomment me for a compilation error!
+
+	// std::tuple<int, MyClass> my_other_tuple = 1;
+
+	// 	Here, the compiler fill in coerccion of int to std::tuple<int, MyClass>
+	// 	without a default constructor for MyClass.
 }
